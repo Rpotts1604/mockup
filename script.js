1d5fe7460a640720dacd12ffd6852780108ec2f8
Riley Potts <riley.potts18@gmail.com>
2023-08-19 15:02:11 +0100

    updated webpage

---
+++ script.js
@@ -0,0 +1,53 @@
+var about = document.getElementsByClassName("about-us-button");
+var services = document.getElementsByClassName("services-button");
+var contact = document.getElementsByClassName("contact-button");
+var feedback = document.getElementsByClassName("feedback-button")
+var i;
+
+for (i = 0; i < about.length; i++) {
+  about[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+for (i = 0; i < services.length; i++) {
+  services[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+for (i = 0; i < contact.length; i++) {
+  contact[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+for (i = 0; i < feedback.length; i++) {
+  feedback[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
